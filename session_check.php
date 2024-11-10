49999dc14413302c0d109b022505a9c7031856aa
Zylei Sugue <zyleisugue@gmail.com>
2024-11-11 03:36:32 +0800

    corrected login link

--- session_check.php
+++ session_check.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is logged in by verifying session variables
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page if user is not logged in
-    header("Location: login.php");
+    header("Location: authentication-login.php");
     exit();
 }
 
