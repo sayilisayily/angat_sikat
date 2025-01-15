db29111f9eb8a0eae6da37661c48cf301dbde4c6
Noely Reyes <145838817+Nowiirii@users.noreply.github.com>
2025-01-15 08:23:43 +0800

    - Responsiveness - alerts - notifications

--- src/organization_management/organizations.php
+++ src/organization_management/organizations.php
@@ -117,6 +117,7 @@ $result = $conn->query($sql);
 </head>
 
 <body>
+    
      <!--  Body Wrapper -->
      <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed">
