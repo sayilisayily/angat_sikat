f7b452d790be893c820947ec7fbc8321fb2812d9
Zylei Sugue <zyleisugue@gmail.com>
2025-01-21 01:44:03 +0800

    datatables

---
+++ src/audit_logs/js/account_logs.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $("#beginningBalanceHistoryTable").DataTable({
+    paging: true,
+    searching: true,
+    info: true,
+    lengthChange: true,
+    pageLength: 10,
+    ordering: true,
+    order: [],
+  });
+
+  $("#cashOnHandHistoryTable").DataTable({
+    paging: true,
+    searching: true,
+    info: true,
+    lengthChange: true,
+    pageLength: 10,
+    ordering: true,
+    order: [],
+  });
+
+  $("#cashOnBankHistoryTable").DataTable({
+    paging: true,
+    searching: true,
+    info: true,
+    lengthChange: true,
+    pageLength: 10,
+    ordering: true,
+    order: [],
+  });
+});
